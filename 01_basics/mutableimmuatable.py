8183cfc7d5ad810683856527349e657328a82811
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-20 13:54:38 +0530

    mutable and immuatables

---
+++ 01_basics/mutableimmuatable.py
@@ -0,0 +1,23 @@
+# this creates a reference which has sushil the refrence now points to username
+
+username = "sushil"
+print(username)
+
+# previous refrence will be deleted after username pointing to another refrence
+
+# this creates another reference which has soumya the refrence now points to username
+
+username = "soumya"
+print(username)
+
+
+
+x = 10
+
+y = x
+print(x)
+print(y)
+ 
+x = 15
+print(x)
+print(y)
