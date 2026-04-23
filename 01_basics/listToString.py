fb4b36534ff9115fe78503847a9ba65bb9203ed1
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-23 22:30:31 +0530

    strings

---
+++ 01_basics/listToString.py
@@ -0,0 +1,40 @@
+
+# list to string
+chai_variety = ["Lemon", "Masala", "Ginger"]
+print(chai_variety)
+print(len(chai_variety))
+print(", ".join(chai_variety))
+
+# string
+chai = "masala chai"
+print(len(chai))
+
+for letter in chai:
+    print(letter)
+
+chai = "he said, \"Masala chai is awsome\""
+print(chai)
+
+chai = "Masala\nchai"
+print(chai)
+
+chai = r"masala\nchai"
+print(chai)
+
+chai = "c:\\user\\pwd"
+print(chai)
+
+chai = r"c:\user\pwd"
+print(chai)
+
+chai = "masala chai"
+print("masal" in chai)
+
+
+
+
+
+
+
+
+
