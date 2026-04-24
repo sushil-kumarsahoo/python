b2d55b59908eadbee66865ab9d1e7f3fc9d79a18
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-25 00:40:16 +0530

    list

--- 01_basics/list.py
+++ 01_basics/list.py
@@ -18,3 +18,33 @@ print(tea_varities)
 tea_varities[1:3] = []    
 print(tea_varities)
 
+for tea in tea_varities:
+ print(tea)
+
+ tea_varities.append("Oolong")
+ print(tea_varities)
+
+ tea_varities.pop()
+ print(tea_varities)
+
+tea_varities.remove("green")
+print(tea_varities)
+
+tea_varities.insert(1,"green")
+print(tea_varities)
+
+# tea_varities_copy = tea_varities  (safe reference)
+
+# copy of tea_varities
+tea_varities_copy = tea_varities.copy()   
+
+tea_varities_copy.append("lemon")
+print(tea_varities_copy)
+
+print(tea_varities)
+
+# list comprehension
+
+square_num = [x**2 for x in range(10)]
+print(square_num)
+
