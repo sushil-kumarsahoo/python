87a002092ae924f9ddcf35ab0af402704d5ce214
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-25 00:14:50 +0530

    lists

---
+++ 01_basics/list.py
@@ -0,0 +1,20 @@
+tea_varities = ["Black","green","oolong","white"]
+
+print(tea_varities)
+
+tea_varities[1:2] = ["lemon"]
+
+print(tea_varities)
+
+tea_varities[1:3] = ["green","masala"]
+print(tea_varities)
+
+tea_varities[1:1]
+print(tea_varities)
+
+tea_varities[1:1] = ["test","test"]
+print(tea_varities)
+
+tea_varities[1:3] = []    
+print(tea_varities)
+
