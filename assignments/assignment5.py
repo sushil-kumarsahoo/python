43cc58d9059d1acdbaaee8202440ba102463c49e
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-26 17:11:23 +0530

    conditional statements

---
+++ assignments/assignment5.py
@@ -0,0 +1,10 @@
+weather = "sunny"
+
+if weather == "sunny":
+   activity = "go for a walk"
+elif weather == "rainy":
+   activity = "read a book"
+elif weather == "snowy":
+  activity = "build a snow man"
+
+print(activity)
---
+++ assignments/assignments4.py
@@ -0,0 +1,10 @@
+fruit = "banana"
+color = "yellow"
+
+if fruit == "banana":
+    if color == "green":
+        print("unripe")
+    elif color == "yellow":
+        print("ripe")   
+    elif color == "brown":
+        print("overripe")
