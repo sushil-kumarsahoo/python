b6cfc380dc42b6afc71b8181288d62b691205ed3
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-28 19:30:13 +0530

    inheritance

--- 07_oop/oop_1.py
+++ 07_oop/oop_1.py
@@ -6,14 +6,24 @@ class Car:
    def full_name(self):
       return f"{self.brand} {self.model}"
       
+class ElectricCar(Car):
+   def __init__(self,brand,model,battery_size):
+      super().__init__(brand,model) 
+      self.battery_size = battery_size
 
 
+my_tesla = ElectricCar("tesla","model s","85kWh") 
+print(my_tesla.model)
+print(my_tesla.full_name())
+print(my_tesla.battery_size) 
 
-my_car = Car("toyota","corolla")     
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
-my_new_car = Car("tata","safari")
-print(my_new_car.brand)
+# my_car = Car("toyota","corolla")     
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+# my_new_car = Car("tata","safari")
+# print(my_new_car.brand)
+
+
 
  
