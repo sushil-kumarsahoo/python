a9eb1150e30c709ee8c4e45cf55fe900b3903725
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-28 18:03:48 +0530

    class and object

--- 07_oop/oop_1.py
+++ 07_oop/oop_1.py
@@ -2,9 +2,18 @@ class Car:
    def  __init__(self,brand,model):
       self.brand = brand
       self.model = model
+    
+   def full_name(self):
+      return f"{self.brand} {self.model}"
+      
+
+
 
 my_car = Car("toyota","corolla")     
 print(my_car.brand)
 print(my_car.model)
+print(my_car.full_name())
 my_new_car = Car("tata","safari")
 print(my_new_car.brand)
+
+ 
